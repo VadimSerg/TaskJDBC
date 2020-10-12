95833573449df7678edb766281cdec38374dff5e
VaDIM <33179761+VadimSerg@users.noreply.github.com>
2020-10-12 23:48:39 +1000

    task1_3

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -4,7 +4,6 @@ import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.util.Util;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,7 +12,6 @@ public class UserDaoHibernateImpl implements UserDao {
 
 
     private final SessionFactory sessionFactory = Util.Hibernateutil.getSessionFactory();
-    private final Session currentSession = sessionFactory.getCurrentSession();
 
     public UserDaoHibernateImpl() {
     }
@@ -45,53 +43,49 @@ public class UserDaoHibernateImpl implements UserDao {
 
     }
 
+
     @Override
     public void saveUser(String name, String lastName, byte age) {
         User user = new User(name, lastName, age);
         new UserDaoHibernateImpl().createUsersTable();
-//        Session session = sessionFactory.openSession();
-//        session.beginTransaction();
-//        session.save(user);
-//        session.getTransaction().commit();
-//        session.close();
-
-        Session current = sessionFactory.getCurrentSession();
-        current.beginTransaction();
-        current.save(user);
-        current.getTransaction().commit();
-
-
+        Session currentSession = sessionFactory.getCurrentSession();
+        currentSession.beginTransaction();
+        currentSession.save(user);
+        currentSession.getTransaction().commit();
     }
+
 
     @Override
     public void removeUserById(long id) {
-        Session session = sessionFactory.openSession();
-        session.beginTransaction();
-        User user = (User) session.get(User.class, id);
-        session.delete(user);
-        session.getTransaction().commit();
-        session.close();
+
+        Session currentSession = sessionFactory.getCurrentSession();
+        currentSession.beginTransaction();
+        User user = (User) currentSession.get(User.class, id);
+        currentSession.delete(user);
+        currentSession.getTransaction().commit();
 
 
     }
 
+
     @Override
     public List<User> getAllUsers() {
-        Session session = sessionFactory.openSession();
-        Transaction transaction = session.beginTransaction();
-        List<User> hibusers = (ArrayList<User>) session.createQuery("from User").list();
-        transaction.commit();
-        session.close();
+
+        Session currentSession = sessionFactory.getCurrentSession();
+        currentSession.beginTransaction();
+        List<User> hibusers =
+                (ArrayList<User>) currentSession.createQuery("from User").list();
+        currentSession.getTransaction().commit();
         return hibusers;
     }
 
     @Override
     public void cleanUsersTable() {
 
-        Session session = sessionFactory.getCurrentSession();
-        session.beginTransaction();
-        session.createQuery("delete from User").executeUpdate();
-        session.getTransaction().commit();
+        Session currentSession = sessionFactory.getCurrentSession();
+        currentSession.beginTransaction();
+        currentSession.createQuery("delete from User").executeUpdate();
+        currentSession.getTransaction().commit();
         System.out.println("HibCleaning users table");
     }
 }
